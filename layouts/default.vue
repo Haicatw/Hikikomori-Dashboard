1d60d8528a9d873343d1fed2dabb55e96ea9525e
kirbyzhou <kirbyzhou@protonmail.com>
2020-07-27 00:14:22 -0700

    Add initial layout and sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,97 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="hidden">
+    <vs-sidebar v-model="active" absolute hover-expand reduce open>
+      <template #logo>
+        <!-- ...img logo -->
+      </template>
+      <vs-sidebar-item id="home">
+        <template #icon>
+          <i class="bx bx-home"></i>
+        </template>
+        Home
+      </vs-sidebar-item>
+      <vs-sidebar-item id="github">
+        <template #icon>
+          <i class="bx bxl-github"></i>
+        </template>
+        Github
+      </vs-sidebar-item>
+      <vs-sidebar-item id="Email">
+        <template #icon>
+          <i class="bx bx-envelope"></i>
+        </template>
+        Email
+      </vs-sidebar-item>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="bx bx-music"></i>
+            </template>
+            Music
+          </vs-sidebar-item>
+        </template>
+        <vs-sidebar-item id="spotify">
+          <template #icon>
+            <i class="bx bxl-spotify"></i>
+          </template>
+          Spotify
+        </vs-sidebar-item>
+        <vs-sidebar-item id="ituns">
+          <template #icon>
+            <i class="bx bxl-apple"></i>
+          </template>
+          Apple Music
+        </vs-sidebar-item>
+      </vs-sidebar-group>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="bx bx-chat"></i>
+            </template>
+            Communication
+          </vs-sidebar-item>
+        </template>
+        <vs-sidebar-item id="discord">
+          <template #icon>
+            <i class="bx bxl-discord"></i>
+          </template>
+          Discord
+        </vs-sidebar-item>
+        <vs-sidebar-item id="slack">
+          <template #icon>
+            <i class="bx bxl-slack"></i>
+          </template>
+          Slack
+        </vs-sidebar-item>
+      </vs-sidebar-group>
+      <template #footer>
+        <vs-row justify="space-between">
+          <vs-avatar badge-color="danger" badge-position="top-right">
+            <i class="bx bx-bell"></i>
+
+            <template #badge>
+              28
+            </template>
+          </vs-avatar>
+
+          <!-- <vs-avatar>
+            <img src="/avatars/avatar-5.png" alt="" />
+          </vs-avatar> -->
+        </vs-row>
+      </template>
+    </vs-sidebar>
+    <nuxt />
   </div>
 </template>
-
+<script>
+export default {
+  data: () => ({
+    active: 'home',
+  }),
+}
+</script>
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
